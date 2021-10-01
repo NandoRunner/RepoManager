98f2a8e5a0690d3f97c351324f311d67643e1807
Fernando Andrade <nando.az@gmail.com>
2021-10-01 19:38:53 -0300

    Setup version implement

--- Code/RepoManager.Desktop/Properties/AssemblyInfo.cs
+++ Code/RepoManager.Desktop/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.21.1")]
-[assembly: AssemblyFileVersion("1.21.1")]
+[assembly: AssemblyVersion("1.21.2")]
+[assembly: AssemblyFileVersion("1.21.2")]
 [assembly: NeutralResourcesLanguage("en")]
